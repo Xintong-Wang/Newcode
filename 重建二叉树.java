f6c6330e88ebe14b34a1094c769ac1a41a33cf03
MegaBrass <51190351+Xintong-Wang@users.noreply.github.com>
2020-05-08 11:58:18 +0800

    Create 重建二叉树.java

---
+++ 重建二叉树.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.Arrays;
+ 
+public class Solution {
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        if(pre.length == 0 || in.length == 0)
+            return null;
+        TreeNode res = new TreeNode(pre[0]);
+        int mid = 0;
+        for(int i = 0; i < in.length; i++){
+            if(in[i] == pre[0]){
+                res.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i + 1), Arrays.copyOfRange(in, 0, i));
+                res.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i + 1, pre.length), Arrays.copyOfRange(in, i + 1, in.length));
+                break;
+            }
+        }
+        return res;
+    }
+}
