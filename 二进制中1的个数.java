98d895637a3fbae25f2ffb14e149588fba9b525e
MegaBrass <51190351+Xintong-Wang@users.noreply.github.com>
2020-05-09 19:08:38 +0800

    Update 二进制中1的个数.java

--- 二进制中1的个数.java
+++ 二进制中1的个数.java
@@ -9,3 +9,15 @@ public class Solution {
         return res;
     }
 }
+
+
+public class Solution {
+    public int NumberOf1(int n) {
+        int res = 0;
+        while(n != 0){
+            res += (n & 1);
+            n >>>= 1;
+        }
+        return res;
+    }
+}
